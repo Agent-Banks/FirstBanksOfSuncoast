ba4b4727fd3272948073430b67e59ba06b1c033e
CodyBanks <codydouglasbanks@gmail.com>
2020-05-23 15:19:19 -0400

    Created two new account objects.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,49 @@ namespace FirstBanksOfSuncoast
 {
     class Program
     {
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var inputFromUser = Console.ReadLine();
+
+            return inputFromUser;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int inputFromUser;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out inputFromUser);
+
+            if (isThisGoodInput)
+            {
+                return inputFromUser;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+
+        }
+
+        static decimal PromptForDecimal(string prompt)
+        {
+            Console.Write(prompt);
+            decimal inputFromUser;
+            var isThisGoodInput = decimal.TryParse(Console.ReadLine(), out inputFromUser);
+
+            if (isThisGoodInput)
+            {
+                return inputFromUser;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+
+        }
         static void Main(string[] args)
         {
             var checkingAccount = new Account()
@@ -20,6 +63,14 @@ namespace FirstBanksOfSuncoast
                 AccountType = "Savings",
                 Transactions = new List<Transaction>()
             };
+
+            var userWantsToQuit = false;
+
+            while (userWantsToQuit == false)
+            {
+                Console.WriteLine("Please choose an option");
+
+            }
         }
     }
 }
